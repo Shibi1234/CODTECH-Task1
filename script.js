13502950ff02a0c1f08883786c97f7e90e45a5a0
Shibi1234 <127323021+Shibi1234@users.noreply.github.com>
2024-07-22 05:20:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 /*USER FRIENDLY TO DO LIST WEB APP- JAVACRIPT FILE*/
+
+let inputBx=document.querySelector('#inputBx');
+let list=document.querySelector('#list');
+console.log(list);
+inputBx.addEventListener("keyup",function(event){
+  if(event.key=="Enter"){
+      addItem(this.value)
+      this.value="";
+  }
+})
+
+
+  let addItem = (inputBx) => {
+      let listItem=document.createElement("li");
+      listItem.innerHTML= `${inputBx}<i></i>`;
+
+    listItem.addEventListener("click",function(){
+     this.classList.toggle('done');
+   })
+   listItem.querySelector("i").addEventListener("click",function()
+   {
+   listItem.remove();
+   })
+   list.appendChild(listItem);
+   }
+
